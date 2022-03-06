77d2bc9348c33f0ad5135000903da1512339c71d
Tolga Boztuna <tolga.boztuna@freebirdairlines.com>
2022-03-07 01:52:34 +0300

    update config source path

--- src/AmqpServiceProvider.php
+++ src/AmqpServiceProvider.php
@@ -34,7 +34,7 @@ class AmqpServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__ . '/../config/amqp.php' => config_path('amqp.php'),
+            __DIR__ . '/config/amqp.php' => config_path('amqp.php'),
         ]);
     }
 
